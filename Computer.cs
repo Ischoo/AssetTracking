d020437524c6b56d45ab8bd0cee0259733c5bef3
Ischoo <isakallanson@gmail.com>
2023-07-26 10:26:29 +0200

    Level 1
    added assets, comp and phones

---
+++ Computer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetTracking
+{
+    internal class Computer : Asset
+    {
+        public Computer(string brand, string model, int price, DateTime date) 
+        {
+            this.model = model;
+            this.brand = brand; 
+            this.price = price;
+            this.purchaseDate = date;
+        }
+    }
+}
